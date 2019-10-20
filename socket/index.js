2530da6f9361651d3dfd44995ea843ba8c63dead
siddhantj929 <siddhantj929@gmail.com>
2019-10-20 13:48:57 +0530

    testing sockets

--- socket/index.js
+++ socket/index.js
@@ -2,6 +2,7 @@ const io = require("socket.io").listen(8000);
 
 io.sockets.on("connection", socket => {
 	socket.on("join", data => {
+		console.log(userId + " Joined");
 		socket.join(data.userId);
 	});
 });
