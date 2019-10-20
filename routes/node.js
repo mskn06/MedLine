590a3da5380637e80ff030c33b07f461bd4a86e4
siddhantj929 <siddhantj929@gmail.com>
2019-10-20 14:17:15 +0530

    tried socket verification

--- routes/node.js
+++ routes/node.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const { addNode, getAllNodes } = require("../services/node");
 const io = require("../socket");
 
+// var email = require("emailjs");
+// var emailClient = email.server.connect({
+// 	user: "siddhantj929@gmail.com",
+// 	password: "a0b112da-a3a7-4cfa-82df-578a558da469",
+// 	host: "smtp.elasticemail.com",
+// 	port: 2525
+// });
+
 router.get("/:userId", async (req, res) => {
 	try {
 		res.status(200).send(await getAllNodes(req.params.userId));
@@ -13,31 +21,43 @@ router.get("/:userId", async (req, res) => {
 
 router.post("/", async (req, res) => {
 	try {
-		const data = await getConfirmation();
+		// server.send(
+		// 	{
+		// 		text: `
+		// 		Diagnosis: ${req.body.diagnosis}
+		// 		Symptoms: ${req.body.symptoms}
+		// 		Prescriptions: ${req.body.prescription}
+		// 	`,
+		// 		from: "Siddhant <siddhantj929@gmail.com>",
+		// 		to: req.body.,
+		// 		cc: "else <else@your-email.com>",
+		// 		subject: "testing emailjs"
+		// 	},
+		// 	function(err, message) {
+		// 		console.log(err || message);
+		// 	}
+		// );
+		const data = await getConfirmation(req.body);
 		if (data) {
-			return res.redirect("/authority/login");
+			res.status(201).send("Confirmed");
 		}
 	} catch (err) {
 		res.status(500).send(err);
 	}
 
-	res.send("Try again");
+	res.send("Try again.");
 });
 
-const getConfirmation = async () => {
+const getConfirmation = async data => {
 	io.sockets
-		.in(req.body.patientId)
-		.emit(
-			"confirm_node",
-			{ msg: "Confirm", data: req.body },
-			async data => {
-				if (!data.isConfirmed) return false;
-				else {
-					const node = await addNode(req.body);
-					return node;
-				}
+		.in(data.patientId)
+		.emit("confirm_node", { msg: "Confirm", data }, async _data => {
+			if (!_data.isConfirmed) return false;
+			else {
+				const node = await addNode(data);
+				return node;
 			}
-		);
+		});
 };
 
 module.exports = router;
