fe4b9ad0ef5f975de59d018a6846ac62d902f147
siddhantj929 <siddhantj929@gmail.com>
2019-10-20 12:50:28 +0530

    changed authority route

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,23 @@
 var express = require("express");
 var router = express.Router();
-const User = require("../models/user")
+const User = require("../models/user");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
 	res.render("home");
 });
 
-router.get("/:authorityId/:userId", async (req, res) => {
+router.get("/visit/:userId", async (req, res) => {
 	try {
-		const user = await User.findById(req.params.userId).populate('nodes');
+		const user = await User.findById(req.params.userId).populate("nodes");
 		res.status(200).render("authority_nodes", {
 			patient: user
-		})
+		});
 	} catch (err) {
 		res.status(500).send({
 			msg: err
-		})
+		});
 	}
-})
-
+});
 
 module.exports = router;
